8f19f710cd168643ae7f6986741021c6e5339f4a
Jamoliddin <jamoliddinqodirov18@gmail.com>
2024-02-11 17:06:17 +0500

    add home page router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,10 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {
+        path: '',
+        loadComponent: () => import('./views/home/home.component').then(m => m.HomeComponent)
+    },
+    {
         path: 'dictionary',
         loadComponent: () => import('./views/dictionary/dictionary.component').then(m => m.DictionaryComponent)
     }
