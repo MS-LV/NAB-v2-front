0099e70b0cffeb3102c25a5993ed102b4748df6a
MS-LV <102213457+MS-LV@users.noreply.github.com>
2024-03-03 07:52:53 +0000

    Deploying to gh-pages from @ MS-LV/NAB-v2-front@3352fa2dbd599d1e5f3dd52decf0edf48e88ce18 🚀

--- chunk-WYKF5BNK.js
+++
@@ -1,1 +0,0 @@
-import{Rb as a,Wb as m,ga as n,ub as o,vb as i}from"./chunk-W63KILHI.js";var l=(()=>{class e{static \u0275fac=function(t){return new(t||e)};static \u0275cmp=n({type:e,selectors:[["app-home"]],standalone:!0,features:[m],decls:2,vars:0,template:function(t,p){t&1&&(o(0,"h1"),a(1,"NAB English Center Jamoliddin"),i())}})}return e})();export{l as HomeComponent};
